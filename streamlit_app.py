fad728eda8741f43a5bd1fe526ec12f0215c261c
Aswini-6 <127596002+Aswini-6@users.noreply.github.com>
2023-03-11 16:07:43 +0530

    Update streamlit_app.py
    added menu details

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 'Breakfast menu'
+
+'Omega 3 & Bluberry Oatmeal'
+'Kale, Spinach & Rocket Smoothie'
+
+'Hard-Boiled Free-Range Egg'
